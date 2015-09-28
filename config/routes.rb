07f2945ec7c3ffb24827f01a3d393f6b49c84b8f
Nick Brennan <nick_p_b@yahoo.com>
2015-09-28 14:03:46 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
- resources :user do
-  resources :posts 
- end
+ 
+  root to: 'welcome#index'
+  get 'sessions/new', to: 'sessions#new', as: 'sign_in'
+  post 'sessions', to: 'sessions#create'
+  delete 'sessions', to: 'sessions#delete'
+
+  resources :user do
+  resources :posts
+  end
+
 end
