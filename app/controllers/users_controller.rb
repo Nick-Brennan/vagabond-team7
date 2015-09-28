2d2b41f9872e0088d1bb519ae37ba1660c472685
Nick Brennan <nick_p_b@yahoo.com>
2015-09-28 15:40:58 -0700

    fixed user_params) '

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
 	private
 
 	def user_params
-		params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
+		params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
 	end
 
 end
