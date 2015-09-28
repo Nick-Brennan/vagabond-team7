0cda42f0c205e41741877062a5bfd49f8a932e9b
phonghoang@gmail.com <phonghoang@gmail.com>
2015-09-28 15:48:04 -0700

    added partials

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   get 'sessions/new', to: 'sessions#new', as: 'sign_in'
   post 'sessions', to: 'sessions#create'
-  delete 'sessions', to: 'sessions#delete'
+  delete 'sessions', to: "sessions#destroy"
 
   resources :users do
   resources :posts
