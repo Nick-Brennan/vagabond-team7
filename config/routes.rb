96635534e54ec1170d7893af3eae5fbea553226b
Eric Euresti <eeuresti@gmail.com>
2015-09-28 13:51:11 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  
-=======
- 
-
-
- resources :user do
+  resources :user do
   resources :posts
-  
- end
->>>>>>> aafcda6e8466dad78210ab480183a4f618d06554
+  end
 end
