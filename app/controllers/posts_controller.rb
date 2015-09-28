c2503be8b9cbd0ba913e53ee8e78049b24d02933
Eric Euresti <eeuresti@gmail.com>
2015-09-28 16:46:31 -0700

    update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,18 @@
 class PostsController < ApplicationController
+    def create
+    end
+
+    def new
+      @article = Article.new
+      @user = current_user
+    end
+
+    def create
+      user = current_user
+      post_params = post.require(:post).permit(:title, :content)
+      post_params[:user_id] = user.id
+      post = Post.create(post_params)
+      redirect_to "/users/#{user.id}"
+    end
+
 end
