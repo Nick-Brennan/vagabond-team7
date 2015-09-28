7ca40b4b7d15797bd6351efd8297ca4269fe7fe9
phonghoang@gmail.com <phonghoang@gmail.com>
2015-09-28 12:39:27 -0700

    added sessions, sessions_helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
  
+  root to: 'welcome#index'
+  get 'sign_in', to: 'sessions#new'
+  post 'sessions', to: 'sessions#create'
+  delete 'sessions', to: 'sessions#delete'
 
-
- resources :user do
+  resources :user do
   resources :posts
-  
- end
+  end
+
 end
