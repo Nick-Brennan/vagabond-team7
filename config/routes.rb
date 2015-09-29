f01e4d3c0ef312007f1b33b736f7c99d05f7cb72
Nick Brennan <nick_p_b@yahoo.com>
2015-09-29 10:34:28 -0700

    added city model and city controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
  
+  get 'cities/index'
+
+  get 'cities/show'
+
   root to: 'welcome#index'
   get '/sessions/new', to: 'sessions#new'
   post '/sessions', to: 'sessions#create'
