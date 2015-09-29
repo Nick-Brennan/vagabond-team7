ec058ab768973999e0f8c3224e009d7da3cf18a5
Nick Brennan <nick_p_b@yahoo.com>
2015-09-28 19:20:08 -0700

    added ul of user's posts to user profile

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
 class PostsController < ApplicationController
 
     def new
-      @article = Article.new
+      @post = Post.new
       @user = current_user
     end
 
     def create
       user = current_user
-      post_params = post.require(:post).permit(:title, :content)
+      post_params = params.require(:post).permit(:title, :content)
       post_params[:user_id] = user.id
       post = Post.create(post_params)
       redirect_to "/users/#{user.id}"
