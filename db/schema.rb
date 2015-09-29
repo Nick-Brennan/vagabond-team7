6f0eec9a7082f4204ade59e644d7605b92598789
Nick Brennan <nick_p_b@yahoo.com>
2015-09-29 10:47:57 -0700

    added cities in seed file

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150929165653) do
+ActiveRecord::Schema.define(version: 20150929173022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.text     "content"
+    t.string   "content"
     t.integer  "user_id"
     t.integer  "city_id"
     t.datetime "created_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+  cities = City.create([{ name: 'San Francisco', image_url: 'http://www.sanfrancisco.travel/sites/sftraveldev.prod.acquia-sites.com/files/SanFrancisco_0.jpg' }, { name: 'London', image_url: 'https://upload.wikimedia.org/wikipedia/commons/6/6e/London_Thames_Sunset_panorama_-_Feb_2008.jpg' }, { name: 'Gibralter', image_url: 'http://www.gibraltarmusicfestival.com/wp-content/uploads/2015/02/GibSunborn.jpg'}])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
