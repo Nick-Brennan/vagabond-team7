a504a80d5b6ec431833c9f533a233486bca20034
justin almeida <justin.h.almeida@gmail.com>
2015-09-29 10:52:22 -0700

    pulling seed file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,11 +34,10 @@ class UsersController < ApplicationController
 	def update
 		user = User.find(params[:id])
 
-		updated_attributes = params.require(:user).permit(:first_name, :last_name,)
+		updated_attributes = params.require(:user).permit(:first_name, :last_name)
 
 		user.update_attribute(:first_name, updated_attributes[:first_name])
 		user.update_attribute(:last_name, updated_attributes[:last_name])
-
 		redirect_to "/users/#{user.id}"
 	end
 
