1cf2aa856d39d5a7a6e6f1cdcc7394840aed9267
Nick Brennan <nick_p_b@yahoo.com>
2015-10-01 10:01:51 -0700

    added Alchemy tags

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
+#typhoeus
+gem 'typhoeus'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,26 @@ class Post < ActiveRecord::Base
 	validates :title, :content, presence: true
 	belongs_to :user
 	belongs_to :city
+
+	attr_accessor :keywords
+  @@alchemy_url ||= 'http://gateway-a.watsonplatform.net/calls/text/TextGetRankedKeywords'
+
+  def get_keywords
+    res = Typhoeus.get(
+      @@alchemy_url,
+      params: {
+        apikey: '5b856d832f158ea06ea555404bf293cb1b0352e4',
+        text: content,
+        maxRetrieve: 10,
+        sentiment: 0,
+        outputMode: "json"
+      }
+    )
+    puts "keyword search: " + res.body.to_s
+    kwords = JSON.parse(res.body)
+    if kwords['keywords'] != nil
+	    words = kwords['keywords'].map { |w| w['text'] }
+	    @keywords = words.join(" | ")
+  	end
+  end
 end
