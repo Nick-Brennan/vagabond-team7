3bd45abcc4bff71a7621a48d981dffc92fea9865
Nick Brennan <nick_p_b@yahoo.com>
2015-09-30 17:21:31 -0700

    aligning city descriptions

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20150930225125) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.text     "content"
+    t.string   "content"
     t.integer  "user_id"
     t.integer  "city_id"
     t.datetime "created_at", null: false
